4347edd557b18533d48d22a048f503e51480e1d9
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-05 21:00:43 +0300

    Add 2 example

---
+++ Example002/Program.cs
@@ -0,0 +1,11 @@
+﻿// на вход принимает целое число Н а на выходе показывает все целые числа от -Н до Н
+int num = 7;
+
+int count = -num;
+
+while (count <= num)
+{
+    Console.Write(count + ", ");
+    count++;
+}
+
