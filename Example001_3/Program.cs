458562595c2202b918c6431aea8b2dad26142286
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-05 21:27:30 +0300

    добавили третий вариант решеня

---
+++ Example001_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// вводит 2 целых числа и проверяет является ли 1ое квадратом 2ого;
+
+int numberA = new Random().Next(1, 60);
+Console.WriteLine(numberA);
+
+int numberB = new Random().Next(1, 60);
+Console.WriteLine(numberB);
+
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine($"{numberA} is quadrat {numberB}");
+} else if(numberB == numberA * numberA)
+{
+    Console.WriteLine($"{numberB} is quadrat {numberA}");
+} else
+{
+    Console.WriteLine("no one number is not a quadrat each other");
+}
