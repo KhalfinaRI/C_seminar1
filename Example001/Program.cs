9e602d5e3338edcffa373930f7281945cd45b5b1
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-05 20:00:59 +0300

    выполнение задачи 1ый способ

---
+++ Example001/Program.cs
@@ -0,0 +1,16 @@
+﻿// вводит 2 целых числа и проверяет является ли 1ое квадратом 2ого;
+
+int numberA = new Random().Next(1, 60);
+Console.WriteLine(numberA);
+
+int numberB = new Random().Next(1, 60);
+Console.WriteLine(numberB);
+
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
